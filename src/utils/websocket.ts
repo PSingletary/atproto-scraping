7bb3a77eec9d7957a1cba89fffde471b715fd795
Mary <git@mary.my.id>
2025-09-28 17:46:30 +0700

    refactor: use partysocket

--- src/utils/websocket.ts
+++ src/utils/websocket.ts
@@ -1,3 +1,5 @@
+import { WebSocket } from 'partysocket';
+
 // deno-lint-ignore no-explicit-any
 export const createWebSocketStream = <T = any>(url: string | URL) => {
 	let ws: WebSocket | undefined;
@@ -5,20 +7,9 @@ export const createWebSocketStream = <T = any>(url: string | URL) => {
 
 	return new ReadableStream<T>({
 		start(controller) {
-			ws = new WebSocket(url);
+			ws = new WebSocket(url.toString());
 			closed = false;
 
-			ws.onclose = (ev) => {
-				if (!closed) {
-					closed = true;
-
-					if (ev.wasClean) {
-						controller.close();
-					} else {
-						controller.error(new Error(`websocket error ${ev.code}: ${ev.reason}`));
-					}
-				}
-			};
 			ws.onmessage = (ev) => {
 				if (!closed) {
 					controller.enqueue(JSON.parse(ev.data));
